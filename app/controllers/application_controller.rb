58bbbf0f1f80c0fdc36deee4d59a9b70c939b0fc
Ermi <manermidem@gmail.com>
2023-05-30 07:48:50 +0300

    Add before_action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user! 
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protect_from_forgery with: :null_session
+    
+
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    end
+
+
 end
