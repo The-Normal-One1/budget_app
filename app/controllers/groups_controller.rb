019b63c58a56a1186e3108a6024429d2e93a1104
Ermi <manermidem@gmail.com>
2023-05-30 22:42:38 +0300

    Add method for the gruops controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,26 @@
 class GroupsController < ApplicationController
 load_and_authorize_resource
+
+def index
+  @catagories = Group.all.where(:user_id => current_user.id).includes(:specifications)
+end
+
+def new
+    @catagories = Group.new
+end 
+
+def create
+    @categories = Group.new(group_params)
+    @categories.user_id = current_user.id
+    if @categories.save
+        redirect_to groups_path, notice: "Category was successfully created"
+    else
+        render 'new'
+    end
+end
+
+private 
+
+def group_params
+    params.require(:group).permit(:name, :icon)
 end
