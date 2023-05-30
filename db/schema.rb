9e63b20569828776ba7589931acc081d2a188882
Ermi <manermidem@gmail.com>
2023-05-30 11:42:05 +0300

    Create specification model for the entity table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_080125) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_081827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_080125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "specifications", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_specifications_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_080125) do
   end
 
   add_foreign_key "groups", "users"
+  add_foreign_key "specifications", "users", column: "author_id"
 end
