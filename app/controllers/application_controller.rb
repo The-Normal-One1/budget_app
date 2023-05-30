85bd9e2a709f36f3a49f80d2edcb77d82d23dbe3
Ermi <manermidem@gmail.com>
2023-05-30 12:01:50 +0300

    Fix linters error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    before_action :authenticate_user! 
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-    protect_from_forgery with: :null_session
-    
+  protect_from_forgery with: :null_session
 
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
     end
-
-
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :groups
-  has_many :specifications, foreign_key: "author_id"
+  has_many :specifications, foreign_key: 'author_id'
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 45, message: "must be between 2 and 20 characters" }
-  validates :email, presence: true, length: { maximum: 255, message: "must be less than 255 characters" }
-  validates :password, presence: true, length: { minimum: 6, maximum: 20, message: "must be between 6 and 20 characters" }
+  validates :name, presence: true, length: { minimum: 2, maximum: 45, message: 'must be between 2 and 20 characters' }
+  validates :email, presence: true, length: { maximum: 255, message: 'must be less than 255 characters' }
+  validates :password, presence: true,
+                       length: { minimum: 6, maximum: 20, message: 'must be between 6 and 20 characters' }
 end
