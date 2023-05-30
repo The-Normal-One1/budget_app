ae98549eccbd42f90aa851cfb97cbd5986c4aed8
Ermi <manermidem@gmail.com>
2023-05-30 23:19:06 +0300

    Edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root to: "home#index"
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "pages#splash"
 
   # # Define routes for the groups
   # resources :groups
