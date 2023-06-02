940a1115b19c8bea51151b160012b0859eab3608
Ermi <manermidem@gmail.com>
2023-06-02 23:52:00 +0300

    Delete extra files

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//=link sessions/new.css
 //=link registrations/new.css
 //=link groups/index.css
 //=link groups/new.css
