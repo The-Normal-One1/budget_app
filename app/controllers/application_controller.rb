c3cc438e8329aadaed8e7eae7498496ebb0f67a7
Ermi <manermidem@gmail.com>
2023-06-02 22:48:53 +0300

    Add after-sign-in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :password_confirmation, :current_password)
     end
   end
+
+  def after_sign_in_path_for(resource)
+    groups_path
+  end
 end
