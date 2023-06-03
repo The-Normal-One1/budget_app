d53a5c9e61851a475ba260cdde83f051999baf23
Ermi <manermidem@gmail.com>
2023-06-03 09:56:44 +0300

    Add system testing for group

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
   # testing devise
   config.include Devise::Test::IntegrationHelpers, type: :request
 
+  config.include Devise::Test::IntegrationHelpers, type: :system
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
---
+++ spec/system/group/index_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups', type: :system do
+
+    let :user do
+        User.find_or_create_by(email: 'budget1234@gmail.com') do |user|
+          user.name = 'Budget'
+          user.password = '1234567'
+          user.confirmed_at = Time.now
+        end
+    end
+
+      let!(:group) { Group.create(name: 'Budget', user_id: user.id, icon: 'icon') }
+        
+        before(:each) do
+            sign_in user
+            visit groups_path
+        end
+
+
+    describe 'GET /index' do
+        it 'returns a list of groups' do
+            visit groups_path
+          expect(page).to have_content('sign in')
+        end
+    
+        it 'renders the correct path' do
+            visit groups_path
+          expect(page).to have_current_path(user_session_path)
+        end 
+      end
+
+        describe 'GET /new' do
+            it 'returns a list file' do
+                visit new_group_path
+              expect(page).to have_content('sign in')
+              end
+        
+            it 'renders the correct path' do
+                visit new_group_path
+              expect(page).to have_current_path(user_session_path)
+            end
+        
+          end
+end
